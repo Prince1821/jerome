4537121e7307d375a43d9fba3f62d7bddcddc53a
princyprince24 <jeromeleoprince12345@gmail.com>
2022-01-27 16:52:01 +0530

    task done

---
+++ src/org/com/Prince.java
@@ -0,0 +1,12 @@
+package org.com;
+
+public class Prince {
+
+public void motor() {
+System.out.println("motor is yemagaa");
+}
+public static void main(String[] args) {
+	Prince p=new Prince();
+	p.motor();
+}
+}
